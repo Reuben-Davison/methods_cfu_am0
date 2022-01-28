05634dee0cf96d3f5955478e502488f18663ee5e
reuben <creubend@gmail.com>
2022-01-27 18:04:40 -0700

    Complete methods

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,71 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
 
+def greeting
+  puts "hello"
+end
+
+greeting
+
+# What is the return value of your method?
+#hello
+# How many arguments did you pass your method?
+#none
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name)
+  puts "#{name}, good afternoon. How are you?"
+
+end
+
+
+custom_greeting("reuben")
+
 
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# puts "#{name}, good afternoon. How are you?"
 
+# How many arguments did you pass your method?
+# 1
+# What data type was your argument(s)?
+# string
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
+def square(number)
+  puts Math.sqrt(number)
+end
+
+square(16.5)
 
 
 
 # What is the return value of your method?
+#the square root of the argument
 # How many arguments did you pass your method?
+#1
 # What data type was your argument(s)?
+#it was an integre but you could also have a float.
 
 
 
 
 #4: Write a method named full name that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
+def full_name(first, middle, last)
+  puts "#{first} #{middle} #{last}"
+end
 
+full_name("reuben", "clark", "davison")
 
 
 # What is the return value of your method?
+# prints first middle and last name
 # How many arguments did you pass your method?
+#3
 # What data type was your argument(s)?
-
+#they were all strings
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+more practice is definitely needed. I struggled way too much over little syntax issues.
 - How confident do you feel writing simple methods of your own?
+The concept is easy to grasp of what is going on, it is the trying to come up with my own solutions that I am struggling with.
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+Once I figure them out, there is really no more questions. I think I am going to need to spend some more time on these though. 
